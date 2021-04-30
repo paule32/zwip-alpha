b3e31bd28c7a3f85aa3fab5bd641feb8263bfcb6
paule32 <kallup.jens@web.de>
2021-04-30 12:59:02 +0200

    update

--- src/inc/error.hpp
+++ src/inc/error.hpp
@@ -73,7 +73,7 @@ namespace kallup::Exception
 	template <auto Type>
 	struct onError {
 		static wchar_t const* toString() {
-			if (std::is_same< Type, ErrorCode >) {
+			if (std::is_same< decltype(Type), ErrorCode >) {
 				// todo: ErrorCode
 			}
 			else {

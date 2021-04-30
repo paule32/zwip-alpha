305a39d1921af797445e152f22f75cbd5bd117b7
paule32 <kallup.jens@web.de>
2021-04-30 13:14:35 +0200

    update

--- src/inc/error.hpp
+++ src/inc/error.hpp
@@ -70,7 +70,7 @@ namespace kallup::Exception
 	class toString {
 		
 	};
-	template <T Type>
+	template <auto Type>
 	struct onError {
 		wchar_t const* toString() {
 			if (std::is_same< Type, ErrorCode >::value ) {
@@ -89,7 +89,7 @@ namespace kallup::Exception
 				// L"unknown:2";
 			}
 		}
-		onError(T Type1, T Type2)
+		onError(Type1, Type2)
 		{
 			if (is_same< Type1, wchar_t >::value
 			&&  is_same< Type2, wchar_t >::value )

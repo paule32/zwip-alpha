2e17fb067ce5f50c3fa5804b621718c8b73dd8db
paule32 <kallup.jens@web.de>
2021-05-02 14:30:54 +0200

    update

--- src/inc/kallup_gui_windows_classic_messagebox.hpp
+++ src/inc/kallup_gui_windows_classic_messagebox.hpp
@@ -86,8 +86,8 @@ namespace kallup::GUI::Windows::Classic
 	// MessageBox< Text, Title, Button >:
 	// ---------------------------------------
 	template <
-		template< typename Ta >, typename Text &,
-		template< typename Tb >, typename Title&, typename Btn& >
+		typename template< typename Ta >, typename Text &,
+		typename template< typename Tb >, typename Title&, typename Btn& >
 	class MessageBox
 	{
 		Text   m_Text;

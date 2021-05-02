204ed40a6035ebfc22b950a3413e609c4866f731
paule32 <kallup.jens@web.de>
2021-05-02 10:06:04 +0200

    update

--- src/gcc/main.cc
+++ src/gcc/main.cc
@@ -42,7 +42,7 @@ std::function<
 int main(void)
 #endif
 {
-	onError<code()>(L"dies ist ein Test", L"Hallo");
+	onError<code(1),text(L"dies ist ein Test"),title(L"Hallo")>();
 	
 	#ifdef TARGET_DLL 
 	return true;

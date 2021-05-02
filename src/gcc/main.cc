ab15de2eb3e95ed5526f512a434ffd6e0ccb106e
paule32 <kallup.jens@web.de>
2021-05-02 10:58:59 +0200

    update

--- src/gcc/main.cc
+++ src/gcc/main.cc
@@ -42,7 +42,7 @@ std::function<
 int main(void)
 #endif
 {
-	onError<kallup::Exception::code(ErrorCode::success),text(L"dies ist ein Test"),title(L"Hallo")>();
+	onError<code(ErrorCode::success),text(L"dies ist ein Test"),title(L"Hallo")>();
 	
 	#ifdef TARGET_DLL 
 	return true;
